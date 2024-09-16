ce6b5639d08b4bf4db319b27971a3cbbbe1f1f08
Rodrigo <rodrigo_atat@hotmail.com>
2024-09-15 23:29:09 -0300

    Cambios

--- server.js
+++ server.js
@@ -142,21 +142,16 @@ app.post('/register', (req, res) => {
   });
 });
 
-// Importar dotenv para leer el archivo .env
-require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
 
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Obtener los secretos de las variables de entorno
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = 'ACab8a6ae0da531c8b4d4113621df604bc'; // Tu Account SID
+const authToken = '92059759ca94a69f60830ed392e439e3'; // Reemplaza con tu Auth Token
 const client = twilio(accountSid, authToken);
 const twilioNumber = 'whatsapp:+14155238886'; // Número de WhatsApp de Twilio
 
